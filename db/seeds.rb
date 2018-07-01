810df3d80372ed9aac6e19369c85f25959de2a00
Suraj Nath <devversitymail@gmail.com>
2018-07-01 22:57:17 +0530

    Add Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Notebook.create!([
+  {id: 1, name: "Notebook 1", desc: "Test Notebook 1"},
+  {id: 2, name: "Notebook 2", desc: "Test Notebook 2"}
+])
+
+Block.create!([
+  {id: 1, notebook_id: 1, name: "Block 1", schema_version: 'v1', data: {}},
+  {id: 2, notebook_id: 1, name: "Block 2", schema_version: 'v1', data: {}},
+  {id: 3, notebook_id: 2, name: "Block 1", schema_version: 'v1', data: {}},
+  {id: 4, notebook_id: 2, name: "Block 2", schema_version: 'v1', data: {}}
+])
